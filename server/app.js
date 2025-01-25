4e86acca6a9fa6beb5971982eb757f52bdd55aa1
Nima sherpa <Toplouie78@gmail.com>
2025-01-25 14:24:18 -0500

    refactor server structure

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const audioRoutes = require("./routes/audioRoutes");
 const app = express();
-const port = 8000;
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -13,6 +14,8 @@ app.get("/", (req, res) => {
 	res.send("Podcast Generator Server is running!");
 });
 
-app.listen(port, () => {
-	console.log(`Server is running at http://localhost:${port}`);
+app.get("*", (req, res) => {
+	res.status(404).send("Page not found");
 });
+
+module.exports = app;
