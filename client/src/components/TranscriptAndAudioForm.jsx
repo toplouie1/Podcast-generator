a8a12fca6d6351d07939c4501deba6437599f6b6
Nima sherpa <Toplouie78@gmail.com>
2025-01-24 19:37:37 -0500

    clean ups / few changes

--- client/src/components/TranscriptAndAudioForm.jsx
+++ client/src/components/TranscriptAndAudioForm.jsx
@@ -3,27 +3,21 @@ import "../css/TranscriptAndAudioForm.css";
 export default function TranscriptAndAudioForm() {
 	return (
 		<div className="left-section">
-			<div className="form-group transcript-group">
-				<label htmlFor="transcript" className="form-label">
-					Transcript:
-				</label>
-				<textarea
-					id="transcript"
-					className="form-control transcript-input"
-					rows="5"
-					placeholder="Enter transcript here"
-				></textarea>
-			</div>
 			<div className="form-group audio-group">
-				<label htmlFor="audio" className="form-label">
-					Audio:
-				</label>
 				<input
 					type="file"
 					id="audio"
 					className="form-control audio-input"
 					accept="audio/*"
 				/>
+			</div>
+			<div className="form-group transcript-group">
+				<textarea
+					id="transcript"
+					className="form-control transcript-input"
+					rows="5"
+					placeholder="Enter transcript here"
+				></textarea>
 			</div>
 			<button className="generate-button">Generate</button>
 		</div>
